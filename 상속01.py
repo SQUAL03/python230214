a4f27b95899a40d2e77d6bc212e156257ccf6ec6
squal03 <squal03@naver.com>
2023-02-15 16:46:52 +0900

    상속연습4

--- 상속01.py
+++ 상속01.py
@@ -21,9 +21,9 @@ class Student(Person):
         self.subject = subject
         self.studentID = studentID
         #재정의(상속받아서 로직에 덮어쓰기)
-        def printInfo(self):
-            print("Info(Name:{0}, Phone Number:{1})".format(self.name,self.phoneNumber))
-            print("Info(Subject:{0}, StudentID:{1})".format(self.subject,self.studentID))
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number:{1})".format(self.name,self.phoneNumber))
+        print("Info(Subject:{0}, StudentID:{1})".format(self.subject,self.studentID))
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "빅데이터", "201234")
 p.printInfo()
