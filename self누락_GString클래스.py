93c18da6edb70cab4e00b7c8ed889331513bed7b
squal03 <squal03@naver.com>
2023-02-15 10:43:22 +0900

    클래스연습3

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,12 +1,15 @@
+# 전역변수(이름충돌)
+# 파이썬은 모호한 것 보다는 명시적인것이 좋다
 str = "Not Class Member"
 class GString:
     def __init__(self):
+        #인스턴스 멤버변수
         self.str = "" 
     def set(self, msg):
         self.str = msg
     def print(self):
-        print(str)
-
+        print(self.str)
+#인스턴스 생성
 g = GString()
 g.set("First Message")
 g.print()
