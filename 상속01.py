90c78450b4862f12aabea6406d79bbf65f29c815
squal03 <squal03@naver.com>
2023-02-15 13:16:52 +0900

    상속연습1

--- 상속01.py
+++ 상속01.py
@@ -4,8 +4,7 @@ class Person(object):
         self.name = name
         self.phoneNumber = phoneNumber
     def printInfo(self):
-        print("Info(Name:{0}, Phone Number: {1})".format(
-            self.name, self.phoneNumber))
+        print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
     def working(self):
         print("현재 작업중...")
     def eating(self):
@@ -24,12 +23,13 @@ class Student(Person):
         #재정의(상속받아서 로직에 덮어쓰기)
         def printInfo(self):
             print("Info(Name:{0}, Phone Number:{1})".format(self.name,self.phoneNumber))
-            print("Info(Subject:{0}, StudentIDr:{1})".format(self.subject,self.studentID))
-
+            print("Info(Subject:{0}, StudentID:{1})".format(self.subject,self.studentID))
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "빅데이터", "201234")
 p.printInfo()
 s.printInfo()
+print(p.__dict__)
+print(s.__dict__)
 s.working()
 s.eating()
 
